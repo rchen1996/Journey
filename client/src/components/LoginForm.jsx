41afa5566ea0e8ba3da6e53831f3bd450c128443
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 13:53:40 -0500

    create login form component with onchange handling

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+export default function LoginForm() {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleForm = event => {
+    event.preventDefault();
+  };
+
+  const handleChange = event => {
+    const { value, name } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  return (
+    <form onSubmit={handleForm}>
+      <input
+        value={userInfo.email}
+        name="email"
+        onChange={handleChange}
+        type="email"
+      />
+      <input
+        value={userInfo.password}
+        name="password"
+        onChange={handleChange}
+        type="password"
+      />
+    </form>
+  );
+}
