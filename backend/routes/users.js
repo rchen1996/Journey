6eaf85fea78efd89ac7b833128406c86c1868d8f
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 10:17:55 -0500

    setup users router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = ({ getUsers, getUserByEmail, addUser }) => {
+  /* GET users listing. */
+  router.get('/', (req, res) => {
+    getUsers()
+      .then(users => res.json(users))
+      .catch(err =>
+        res.json({
+          error: err.message,
+        })
+      );
+  });
 
-module.exports = router;
+  router.post('/', (req, res) => {
+    const { first_name, last_name, email, password } = req.body;
+
+    getUserByEmail(email)
+      .then(user => {
+        if (user) {
+          res.json({
+            msg: 'Sorry, a user account with this email already exists',
+          });
+        } else {
+          return addUser(first_name, last_name, email, password);
+        }
+      })
+      .then(newUser => res.json(newUser))
+      .catch(err =>
+        res.json({
+          error: err.message,
+        })
+      );
+  });
+
+  return router;
+};
