a655b8c55529b7908360223ee839c1ebb0a0faa4
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 18:15:38 -0500

    create component for itinerary item in list view show

---
+++ client/src/components/ItineraryListItem.jsx
@@ -0,0 +1,10 @@
+export default function ItineraryListItem(props) {
+  return (
+    <div>
+      {/* itinerary image */}
+      <h4>Itinerary Name</h4>
+      <p>Itinerary Description</p>
+      {/* bookmark button */}
+    </div>
+  );
+}
