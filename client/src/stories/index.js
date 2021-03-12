2b88608c1a54ed52d46f63a7874c13f61d9cd9d5
May C <maycheongs@gmail.com>
2021-03-12 13:51:56 -0500

    create basic register component

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -4,5 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Nav from '../components/Nav';
+import RegisterForm from '../components/RegisterForm'
 
 storiesOf('Nav', module).add('Default', () => <Nav />);
+
+
+storiesOf('RegisterForm',module).add('Empty',()=> (
+<RegisterForm
+register={action('register')}
+/>
+))
