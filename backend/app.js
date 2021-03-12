8d05fdb2daad176105a52001426cf03b583bbf27
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 11:58:05 -0500

    update import of db helpers to now use api and users helpers

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/itineraries');
 
 const db = require('./db');
-const dbHelpers = require('./helpers/dbHelpers')(db);
+const userHelpers = require('./helpers/userHelpers')(db);
+const apiHelpers = require('./helpers/apiHelpers')(db);
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/users', usersRouter(dbHelpers));
+app.use('/api/users', usersRouter(userHelpers));
+app.use('/api/itineraries', apiRouter(apiHelpers));
 
 module.exports = app;
