54ded85bb7618e38512d9c854a6a7d822d481d94
Rebecca Chen <rebecca061996@gmail.com>
2021-03-11 20:17:44 -0500

    create seeds for locations table

---
+++ backend/db/seeds/04_locations.sql
@@ -0,0 +1,10 @@
+INSERT INTO locations (name, country, latitude, longitude)
+VALUES ('Dublin', 'IRL', 53.3498, -6.2603),
+('Paris', 'FRA', 48.8566, 2.3522),
+('Venice', 'ITA', 45.4408, 12.3155),
+('Rome', 'ITA', 41.9028, 12.4964),
+('Amsterdam', 'NLD', 52.3676, 4.9041),
+('Tokyo', 'JPN', 35.6762, 139.6503),
+('Kyoto', 'JPN', 35.0116, 135.7681),
+('North Cape', 'NOR', 71.1710, 25.7837),
+('Aspen', 'USA', 39.1911, -106.8175);
