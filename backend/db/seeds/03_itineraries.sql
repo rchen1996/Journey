d80000bff4f439308ed1caee209b917f8ee395cd
Rebecca Chen <rebecca061996@gmail.com>
2021-03-11 22:21:28 -0500

    update seeds to include an image and remove days array after db schema update

--- backend/db/seeds/03_itineraries.sql
+++ backend/db/seeds/03_itineraries.sql
@@ -1,6 +1,6 @@
-INSERT INTO itineraries (name, description,days,trip_type,user_id)
-VALUES ('My Ireland Trip','Lots of stout and leprechauns in our future',[1,2,3,4,5,6],'groups',1),
-('Our Honeymoon in Europe','Tu est francais?',[1,2,3,4,5,6,7,8,9,10,11,12],'couples',2),
-('Japan!','Food and Shopping',[1,2,3,4,5],'families',3),
-('My Norway Getaway','Northern liiiightsssss',[1,2,3,4,5,6],'solo',1),
-('Aspen','Skiing and shopping in the village',[1,2,3,4,5,6,7,8],'luxury',4);
+INSERT INTO itineraries (name, description, image, trip_type, creator_id)
+VALUES ('My Ireland Trip','Lots of stout and leprechauns in our future', 'https://images.adsttc.com/media/images/5bd6/fd00/f197/ccaa/4a00/027c/newsletter/shutterstock_1116656705.jpg?1540816099', 'groups', 1),
+('Our Honeymoon in Europe','Tu est francais?','https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900','couples',2),
+('Japan!','Food and Shopping','https://cdn-image.departures.com/sites/default/files/1576002985/header-tokyo-japan-LUXETOKYO1219.jpg','families',3),
+('My Norway Getaway','Northern liiiightsssss','https://cdn.kimkim.com/files/a/content_articles/featured_photos/91f73b325868f65a810731f7277b731a553db51f/big-1e3cac6bb208d30558f1623b379e121a.jpg','solo',1),
+('Aspen','Skiing and shopping in the village','https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d1573f34c687b00085b1145%2F0x0.jpg','luxury',4);
