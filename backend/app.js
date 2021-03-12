afcbab1c27e7892b22683967f3f588cdc9ff288c
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 13:38:23 -0500

    require cookie session and remove cookie parser

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
@@ -12,10 +12,15 @@ const apiHelpers = require('./helpers/apiHelpers')(db);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/users', usersRouter(userHelpers));
