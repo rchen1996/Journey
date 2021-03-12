9a6979a83f106b401e3dc282bd7d430c459e26f1
Rebecca Chen <rebecca061996@gmail.com>
2021-03-11 23:37:35 -0500

    update some itineraries to include start and end dates

--- backend/db/seeds/02_itineraries.sql
+++ backend/db/seeds/02_itineraries.sql
@@ -1,6 +1,8 @@
 INSERT INTO itineraries (name, description, image, trip_type, creator_id)
 VALUES ('My Ireland Trip','Lots of stout and leprechauns in our future', 'https://images.adsttc.com/media/images/5bd6/fd00/f197/ccaa/4a00/027c/newsletter/shutterstock_1116656705.jpg?1540816099', 'groups', 1),
 ('Our Honeymoon in Europe','Tu est francais?','https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900','couples',2),
-('Japan!','Food and Shopping','https://cdn-image.departures.com/sites/default/files/1576002985/header-tokyo-japan-LUXETOKYO1219.jpg','families',3),
-('My Norway Getaway','Northern liiiightsssss','https://cdn.kimkim.com/files/a/content_articles/featured_photos/91f73b325868f65a810731f7277b731a553db51f/big-1e3cac6bb208d30558f1623b379e121a.jpg','solo',1),
-('Aspen','Skiing and shopping in the village','https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d1573f34c687b00085b1145%2F0x0.jpg','luxury',4);
+('Japan!','Food and Shopping','https://cdn-image.departures.com/sites/default/files/1576002985/header-tokyo-japan-LUXETOKYO1219.jpg','families',3);
+
+INSERT INTO itineraries (name, description, image, trip_type, creator_id, start_date, end_date)
+VALUES('My Norway Getaway','Northern liiiightsssss','https://cdn.kimkim.com/files/a/content_articles/featured_photos/91f73b325868f65a810731f7277b731a553db51f/big-1e3cac6bb208d30558f1623b379e121a.jpg','solo',1, '2021-10-10','2021-10-16'),
+('Aspen','Skiing and shopping in the village','https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d1573f34c687b00085b1145%2F0x0.jpg','luxury',4,'2021-12-20','2021-12-28');
