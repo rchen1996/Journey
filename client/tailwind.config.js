57211f3a2b4fb8d2649b8dc9bfd712124c888e6e
babs20 <thevillemc@gmail.com>
2021-03-12 10:04:25 -0500

    add plugins to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,5 +12,9 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
