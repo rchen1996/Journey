c34650915d838aebbec1ec9c7618eeaf70a0972d
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 12:12:58 -0500

    initial setup for react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import useApplicationData from './hooks/useApplicationData';
 
 function App() {
   const { state, dispatch } = useApplicationData();
-  const userList = state.users.map((user) => (
+  const userList = state.users.map(user => (
     <li key={user.id}>
       {' '}
       {user.first_name} {user.last_name} {user.email}{' '}
     </li>
   ));
   return (
-    <div className=''>
-      <h1 className='text-xl text-teal-600'> Users </h1>
+    <div className="">
+      <h1 className="text-xl text-teal-600"> Users </h1>
 
       <ul> {userList} </ul>
     </div>
