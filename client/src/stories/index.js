5db96f7ef4201ba1f9fec7d4982fc584d0a064a6
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 13:21:31 -0500

    add stories for nav button

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -4,5 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Nav from '../components/Nav';
+import NavButton from '../components/NavButton';
 
 storiesOf('Nav', module).add('Default', () => <Nav />);
+
+storiesOf('NavButton', module)
+  .add('Dashboard', () => <NavButton>Dashboard</NavButton>)
+  .add('Log out', () => <NavButton>Log out</NavButton>)
+  .add('Clickable', () => (
+    <NavButton onClick={action('button-cicked')}>Clickable</NavButton>
+  ));
