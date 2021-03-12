108862ea46fea011430b7755bc35e43630e59c71
Rebecca Chen <rebecca061996@gmail.com>
2021-03-11 22:36:04 -0500

    complete seeds for attractions table

--- backend/db/seeds/05_attractions.sql
+++ backend/db/seeds/05_attractions.sql
@@ -2,15 +2,16 @@ INSERT INTO attractions (name,description,category,image,location)
 VALUES ('Guiness Storehouse', 'Free Stout for days','adult','https://lh5.googleusercontent.com/p/AF1QipOBu91VD75j_3kjb8Bl8MzFF3BPXA8JFuvlNAmI=w408-h358-k-no','53.3419812,-6.2889051'),
 
 ('Phoenix Park', 'mature trees and deer','nature','https://lh5.googleusercontent.com/p/AF1QipOfy-78jvDcn7do6MWRsvIH4zuJJYsUUq3Qx6ZA=w408-h725-k-no','53.3637092,-6.345843'),
+('The Shelbourne, Autograph Collection', 'hotel featuring plush rooms and elegant furnishings','accomodation','https://lh5.googleusercontent.com/p/AF1QipO4J4CWMWKmStq74vTF_Pnk0E0eT2uSFkBpXtmB=w408-h272-k-no','53.3389757,-6.2582078'),
 
-('Eiffel Tower', 'must-see','landmarks','https://lh5.googleusercontent.com/p/AF1QipMFvt9DUkfvGQkhDUTQK5mUFTKAGwTzp3RqSpAG=w408-h295-k-no','48.8589506,2.2768486'),
-('Louvre Museum', 'must-see','landmarks','https://lh5.googleusercontent.com/p/AF1QipNdOTCYdCwL355cXFXmv9t1wSLg8Y7caUjhw2Kf=w408-h261-k-no','48.8692834,2.3074043'),
+('Eiffel Tower', 'must-see','landmark','https://lh5.googleusercontent.com/p/AF1QipMFvt9DUkfvGQkhDUTQK5mUFTKAGwTzp3RqSpAG=w408-h295-k-no','48.8589506,2.2768486'),
+('Louvre Museum', 'must-see','landmark','https://lh5.googleusercontent.com/p/AF1QipNdOTCYdCwL355cXFXmv9t1wSLg8Y7caUjhw2Kf=w408-h261-k-no','48.8692834,2.3074043'),
 
 
-('Saint Mark''s Basilica', 'must-see','landmarks','https://lh5.googleusercontent.com/p/AF1QipObLVRR6BrEMxJXpve5XB4QukFCLRqXcr6V62xA=w408-h306-k-no','45.424581,12.2687073'),
-('Bridge of Sighs', 'must-see','landmarks','https://lh5.googleusercontent.com/p/AF1QipM3O4UewL5HOnavykbEYhhY4jS6qkUd_B4JyV2n=w408-h306-k-no','45.4340533,12.3386604'),
+('Saint Mark''s Basilica', 'must-see','landmark','https://lh5.googleusercontent.com/p/AF1QipObLVRR6BrEMxJXpve5XB4QukFCLRqXcr6V62xA=w408-h306-k-no','45.424581,12.2687073'),
+('Bridge of Sighs', 'must-see','landmark','https://lh5.googleusercontent.com/p/AF1QipM3O4UewL5HOnavykbEYhhY4jS6qkUd_B4JyV2n=w408-h306-k-no','45.4340533,12.3386604'),
 
-('Colosseum', 'must-see','landmarks','https://lh5.googleusercontent.com/p/AF1QipM3O4UewL5HOnavykbEYhhY4jS6qkUd_B4JyV2n=w408-h306-k-no','45.4340533,12.3386604'),
+('Colosseum', 'must-see','landmark','https://lh5.googleusercontent.com/p/AF1QipM3O4UewL5HOnavykbEYhhY4jS6qkUd_B4JyV2n=w408-h306-k-no','45.4340533,12.3386604'),
 ('Hostaria al Boschetto', 'popular restaurant','food','https://lh5.googleusercontent.com/p/AF1QipPM4N3bc_Y736QLztEh4ifeJwFM-R8Nf2YefF2-=w408-h293-k-no','41.8925374,12.4872704'),
 ('Terrazza Les Étoiles Roma', 'fine dining restaurant','food','https://lh5.googleusercontent.com/p/AF1QipPdXKgLMBGWTXOtVUtJrpmJ4F-YYPzFI52R5d16=w408-h270-k-no','41.8952448,12.4616392'),
 
@@ -20,5 +21,11 @@ VALUES ('Guiness Storehouse', 'Free Stout for days','adult','https://lh5.googleu
 ('Tokyo Disneyland', 'Mickey Mouse speaks japanese','amusement','https://lh5.googleusercontent.com/p/AF1QipOQqhceiyC1qq4AuiX_-I2i-9wVH31DD-i8Ss6b=w408-h306-k-no','35.6329007,139.8782003'),
 ('Tokyo Skytree', 'World''s tallest freestanding broadcasting tower with an observation deck boasting 360-degree views.','landmarks','https://lh5.googleusercontent.com/p/AF1QipMZOND0BcUzqWzLKxntdXNq50vBbkP2ZRAAU4li=w408-h444-k-no','35.7100627,139.7406626'),
 
-('Kinkaku-ji', 'Iconic golden temple','cultural','https://lh5.googleusercontent.com/p/AF1QipPjKak8aP8ew2EDy2YDZrD74dHbLTaLeAcEx1Yp=w408-h306-k-no','35.03937,135.7292431');
+('Kinkaku-ji', 'Iconic golden temple','cultural','https://lh5.googleusercontent.com/p/AF1QipPjKak8aP8ew2EDy2YDZrD74dHbLTaLeAcEx1Yp=w408-h306-k-no','35.03937,135.7292431'),
+('Jidaiya', 'Popular izakaya','food','https://lh5.googleusercontent.com/p/AF1QipNW5M4BvpNwa3q_HxFKAsVYObwzsthFP3XsIHQf=w408-h544-k-no','34.9638035,135.7684177'),
 
+('Kirkeporten in Skarsvåg', 'Nature preserve','nature','https://lh5.googleusercontent.com/p/AF1QipPjFRRAVPfDIrErQydSW1aEdqFQGHS8IwkNtr-I=w408-h251-k-no','71.1060835,25.8008152'),
+('Cape Marina Fishing Camp', 'fishing camp with organised fishing trips','nature','https://lh5.googleusercontent.com/p/AF1QipMkL1xpfgfsCWyYdhbsciU_OQmac8qVruAPgVkf=w426-h240-k-no','71.1060835,25.8008152'),
+
+('Aspen Mountain Ski Resort', 'Well-known alpine recreation area with 64 mi. of ski trails, upscale hotels & scenic dining.','sport','https://lh5.googleusercontent.com/p/AF1QipO70P-CKDmwT8O4_-c01qiHoAzMHC1Igkim2IA0=w408-h306-k-no','39.1862642,-106.820421'),
+('Aspen Mountain Lodge', 'high-end lodges with private chefs','accomodation','https://lh6.googleusercontent.com/proxy/1VHyMyEk1yx8k7fdKlAmeJNTp76tJs3piU8WZ9B5PxdHyOoFGAy5DzIS3QnJ1RsTXcxorEzJYM8hbCdkm_SQ2s3B6PJBM-44Iq0Ai4QGiaxsL5VRhJ3wwo20eF_o49mCnaCYtE3tlFeGgDo-F8sP-w49Kz1nog=w408-h274-k-no','39.1854534,-106.8243424');
