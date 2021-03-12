3def8fe33fac43c11558ccda12728ceaa9967361
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 14:42:32 -0500

    add route for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,30 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import useApplicationData from './hooks/useApplicationData';
 import Home from './components/Home';
+import LoginForm from './components/LoginForm';
+
 function App() {
-  const { state, dispatch } = useApplicationData();
-  const userList = state.users.map((user) => (
+  const { state, dispatch, login } = useApplicationData();
+  const userList = state.users.map(user => (
     <li key={user.id}>
       {' '}
       {user.first_name} {user.last_name} {user.email}{' '}
     </li>
   ));
+
   return (
     <Router>
-      <div className=''>
-        <h1 className='text-xl text-teal-600 font-body'> Users </h1>
-
-        <ul className='font-body'> {userList} </ul>
+      <div className="">
+        <h1 className="text-xl text-teal-600 font-body"> Users </h1>
+        <ul className="font-body"> {userList} </ul>
       </div>
       <nav></nav>
       <Switch>
-        <Route path='/'>
+        <Route path="/" exact>
           <Home />
+        </Route>
+        <Route path="/login">
+          <LoginForm onSave={login} />
         </Route>
       </Switch>
     </Router>
