b0231509ed21ac054cc78c6cc6a8d66e790f713b
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 14:24:37 -0500

    fix form to handle save on submit

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -19,7 +19,9 @@ export default function LoginForm(props) {
     setUserInfo({ ...userInfo, [name]: value });
   };
 
-  const save = () => {
+  const save = event => {
+    event.preventDefault();
+
     if (userInfo.email === '') {
       setError('Email cannot be blank');
       return;
@@ -48,10 +50,7 @@ export default function LoginForm(props) {
   return (
     <section className="w-full shadow-lg bg-gray-50 rounded-xl">
       <div>{error}</div>
-      <form
-        onSubmit={event => event.preventDefault()}
-        className="flex flex-col"
-      >
+      <form onSubmit={event => save(event)} className="flex flex-col">
         <div className="flex flex-col mx-8 my-6">
           <label htmlFor="email" className="font-semibold">
             Email
@@ -76,16 +75,17 @@ export default function LoginForm(props) {
             className="mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
           />
         </div>
+
+        <footer className="flex items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 rounded-b-xl">
+          <span className="text-xs font-semibold">
+            Don't have an account? Sign up{' '}
+            <Link to="/signup" className="text-teal-600 hover:underline">
+              here!
+            </Link>
+          </span>
+          <FormButton>Log in</FormButton>
+        </footer>
       </form>
-      <footer className="flex items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 rounded-b-xl">
-        <span className="text-xs font-semibold">
-          Don't have an account? Sign up{' '}
-          <Link to="/signup" className="text-teal-600 hover:underline">
-            here!
-          </Link>
-        </span>
-        <FormButton onClick={save}>Log in</FormButton>
-      </footer>
     </section>
   );
 }
