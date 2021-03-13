20fb877ae1b2b6abbbaf395305a9cee8bf04e199
babs20 <thevillemc@gmail.com>
2021-03-12 20:48:52 -0500

    add styles to log in form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,9 +2,11 @@ import LoginForm from './LoginForm';
 
 export default function Login(props) {
   return (
-    <div>
-      <h1>Welcome Back!</h1>
-      <LoginForm onSave={props.onSave} dispatch={props.dispatch} />
+    <div className='flex flex-col items-center w-full h-full'>
+      <div className='flex flex-col w-1/2 mt-32'>
+        <h1 className='text-2xl font-bold mb-4 ml-1'>Welcome Back!</h1>
+        <LoginForm onSave={props.onSave} dispatch={props.dispatch} />
+      </div>
     </div>
   );
 }
