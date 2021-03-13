1e24b159a12f892822aa8a79ce965a9e1864d84d
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 13:23:34 -0500

    update homepage text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home(props) {
   return (
     <div>
-      <h1 className="text-teal-600">Welcome to Journey</h1>
+      <h1 className="text-teal-600">Go on an</h1>
+      <h1 className="text-teal-600">adventure!</h1>
       <p>
         Journey is a group trip planning app, that allows users to create
         itineraries with the other members in their group, in real time. Pick a
