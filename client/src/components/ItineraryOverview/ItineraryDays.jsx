6112121637bc3301342f07cc01f538e1dbec247a
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 16:55:25 -0500

    add .map to render each activity for a specific day

--- client/src/components/ItineraryOverview/ItineraryDays.jsx
+++ client/src/components/ItineraryOverview/ItineraryDays.jsx
@@ -1,9 +1,15 @@
+import ItineraryDayActivities from './ItineraryDayActivities';
+
 export default function ItineraryDays(props) {
   const { day } = props;
 
   return (
     <div>
       <h2>Day {day.day_order}</h2>
+      {day.activities &&
+        day.activities.map(activity => {
+          return <ItineraryDayActivities key={activity.id} />;
+        })}
     </div>
   );
 }
