a0c57ad7b7c87af65d2a3469f6f7439a5791293a
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 17:06:45 -0500

    create component for each activity in a day for an itinerary overview

--- client/src/components/ItineraryOverview/ItineraryDayActivities.jsx
+++ client/src/components/ItineraryOverview/ItineraryDayActivities.jsx
@@ -1,1 +1,17 @@
-export default function ItineraryDayActivities(props) {}
+export default function ItineraryDayActivities(props) {
+  const { activity } = props;
+
+  console.log(activity);
+
+  return (
+    <div>
+      <h4>{activity.name}</h4>
+      <p>location</p>
+      {activity.start_time && activity.end_time && (
+        <p>
+          {activity.start_time} - {activity.end_time}
+        </p>
+      )}
+    </div>
+  );
+}
