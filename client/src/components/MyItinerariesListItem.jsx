070bbc78c87a6d63c80933a5a2eb5514e53751f6
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 23:04:49 -0500

    create component for my itinerary list item

--- client/src/components/MyItinerariesListItem.jsx
+++ client/src/components/MyItinerariesListItem.jsx
@@ -1,3 +1,11 @@
 export default function MyItinerariesListItem(props) {
-  return <p>Hello</p>;
+  const { name, description, image } = props.itinerary;
+  return (
+    <div>
+      <img src={image} alt="itinerary cover" />
+      <h4>{name}</h4>
+      <p>{description}</p>
+      {/* add edit and delete buttons */}
+    </div>
+  );
 }
