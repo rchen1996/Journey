aae169ca1e2a31653fd3f3b8b4b0d3bd75c0a6fb
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 10:42:51 -0500

    create new helper to create a new travel party for an itinerary

--- backend/helpers/apiHelpers.js
+++ backend/helpers/apiHelpers.js
@@ -12,7 +12,8 @@ module.exports = db => {
 
   const createNewItinerary = itinerary => {
     const query = {
-      text: `INSERT INTO itineraries (name, description, image, trip_type, creator_id, start_date, end_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
+      text: `INSERT INTO itineraries (name, description, image, trip_type, creator_id, start_date, end_date) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
       values: [
         itinerary.name,
         itinerary.description,
@@ -30,8 +31,22 @@ module.exports = db => {
       .catch(err => err);
   };
 
+  const createTravelParty = (itineraryId, userId) => {
+    const query = {
+      text: `INSERT INTO travel_parties (itinerary_id, user_id)
+      VALUES($1, $2) RETURNING *;`,
+      values: [itineraryId, userId],
+    };
+
+    return db
+      .query(query)
+      .then(result => result.rows[0])
+      .catch(err => err);
+  };
+
   return {
     getAllItineraries,
     createNewItinerary,
+    createTravelParty,
   };
 };
