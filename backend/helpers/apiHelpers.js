9d4f65d0fc584dbd82b546c041c988cbebf5872e
Rebecca Chen <rebecca061996@gmail.com>
2021-03-12 21:13:57 -0500

    create function to insert new itinerary into db

--- backend/helpers/apiHelpers.js
+++ backend/helpers/apiHelpers.js
@@ -10,7 +10,36 @@ module.exports = db => {
       .catch(err => err);
   };
 
+  const createNewItinerary = (
+    name,
+    description,
+    image,
+    tripType,
+    creator_id,
+    startDate,
+    endDate
+  ) => {
+    const query = {
+      text: `INSERT INTO itineraries (name, description, image, trip_type, creator_id, start_date, end_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
+      values: [
+        name,
+        description,
+        image,
+        tripType,
+        creator_id,
+        startDate,
+        endDate,
+      ],
+    };
+
+    return db
+      .query(query)
+      .then(result => result.rows)
+      .catch(err => err);
+  };
+
   return {
     getAllItineraries,
+    createNewItinerary,
   };
 };
