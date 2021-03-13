8a21981d3f5790b4cd50d9f1c58ca3df7fb40d7c
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 17:29:21 -0500

    update time for each activity to use AM/PM and not show seconds

--- client/src/components/ItineraryOverview/ItineraryDayActivities.jsx
+++ client/src/components/ItineraryOverview/ItineraryDayActivities.jsx
@@ -1,7 +1,29 @@
 export default function ItineraryDayActivities(props) {
   const { activity } = props;
 
-  console.log(activity);
+  const tConvert = time => {
+    // Check correct time format and split into components
+    time = time
+      .toString()
+      .match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+    let timeArr = [];
+
+    if (time.length > 1) {
+      // If time format correct
+      time = time.slice(1); // Remove full string match value
+      time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+      time[0] = +time[0] % 12 || 12; // Adjust hours
+
+      for (let i = 0; i < time.length; i++) {
+        if (i !== 3) {
+          timeArr.push(time[i]);
+        }
+      }
+    }
+
+    return timeArr.join(''); // return adjusted time or original string
+  };
 
   return (
     <div>
@@ -9,7 +31,7 @@ export default function ItineraryDayActivities(props) {
       <p>location</p>
       {activity.start_time && activity.end_time && (
         <p>
-          {activity.start_time} - {activity.end_time}
+          {tConvert(activity.start_time)} - {tConvert(activity.end_time)}
         </p>
       )}
     </div>
