18565a0134ec13b92372ee69a9ad4f724c065b88
Rebecca Chen <rebecca061996@gmail.com>
2021-03-14 12:21:24 -0400

    add start and end time to form and create state to handle form input with onchange handler

--- client/src/components/ItineraryDay/AddActivityForm.jsx
+++ client/src/components/ItineraryDay/AddActivityForm.jsx
@@ -5,10 +5,10 @@ import ErrorMessage from '../ErrorMessage';
 import FormButton from '../FormButton';
 
 export default function AddActivityForm(props) {
-  // const [userInfo, setUserInfo] = useState({
-  //   email: '',
-  //   password: '',
-  // });
+  const [activity, setActivity] = useState({
+    start: '',
+    end: '',
+  });
 
   const [error, setError] = useState({
     staus: false,
@@ -19,55 +19,56 @@ export default function AddActivityForm(props) {
 
   const history = useHistory();
 
-  // const handleChange = event => {
-  //   const { value, name } = event.target;
-  //   setUserInfo({ ...userInfo, [name]: value });
-  // };
+  const handleChange = event => {
+    let { value, name } = event.target;
+    setActivity({ ...activity, [name]: value });
+  };
 
-  // const save = event => {
-  //   event.preventDefault();
+  const save = event => {
+    event.preventDefault();
 
-  //   if (userInfo.email === '') {
-  //     setError({
-  //       ...error,
-  //       status: true,
-  //       message: 'Please enter an email.',
-  //     });
-  //     return;
-  //   }
+    console.log(activity);
+    // if (userInfo.email === '') {
+    //   setError({
+    //     ...error,
+    //     status: true,
+    //     message: 'Please enter an email.',
+    //   });
+    //   return;
+    // }
 
-  //   if (userInfo.password === '') {
-  //     setError({
-  //       ...error,
-  //       status: true,
-  //       message: 'Please enter a password.',
-  //     });
-  //     return;
-  //   }
+    // if (userInfo.password === '') {
+    //   setError({
+    //     ...error,
+    //     status: true,
+    //     message: 'Please enter a password.',
+    //   });
+    //   return;
+    // }
 
-  //   props.onSave(userInfo.email, userInfo.password).then(res => {
-  //     if (res.data.email) {
-  //       setError({
-  //         ...error,
-  //         status: false,
-  //         message: '',
-  //       });
+    // props.onSave(userInfo.email, userInfo.password).then(res => {
+    //   if (res.data.email) {
+    //     setError({
+    //       ...error,
+    //       status: false,
+    //       message: '',
+    //     });
 
-  //       props.dispatch({
-  //         type: SET_USER,
-  //         user: res.data,
-  //       });
+    //     props.dispatch({
+    //       type: SET_USER,
+    //       user: res.data,
+    //     });
 
-  //       history.push(`/dashboard/${res.data.id}`);
-  //     } else if (res.data.error) {
-  //       setError({
-  //         ...error,
-  //         status: true,
-  //         message: 'Incorrect email or password.',
-  //       });
-  //     }
-  //   });
-  // };
+    //     history.push(`/dashboard/${res.data.id}`);
+    //   } else if (res.data.error) {
+    //     setError({
+    //       ...error,
+    //       status: true,
+    //       message: 'Incorrect email or password.',
+    //     });
+    //   }
+    // });
+  };
 
   return (
     <section className="w-full shadow-lg bg-gray-50 rounded-xl">
@@ -77,10 +78,7 @@ export default function AddActivityForm(props) {
         hide={error.hide}
         message={error.message}
       ></ErrorMessage>
-      <form
-        onSubmit={event => event.preventDefault()}
-        className="flex flex-col"
-      >
+      <form onSubmit={event => save(event)} className="flex flex-col">
         <div className="flex flex-col mx-8 my-6">
           <label htmlFor="email" className="font-semibold">
             Email
@@ -89,19 +87,28 @@ export default function AddActivityForm(props) {
             value={1}
             name="email"
             onChange={() => console.log('here')}
-            type="email"
+            type="text"
             placeholder="Email"
             className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
           />
-          <label htmlFor="password" className="font-semibold">
-            Password
+          <label htmlFor="start" className="font-semibold">
+            Start Time
           </label>
           <input
-            value={1}
-            name="password"
-            onChange={() => console.log('here')}
-            type="password"
-            placeholder="Password"
+            value={activity.start}
+            name="start"
+            onChange={event => handleChange(event)}
+            type="time"
+            className="mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
+          />
+          <label htmlFor="end" className="font-semibold">
+            End Time
+          </label>
+          <input
+            value={activity.end}
+            name="end"
+            onChange={event => handleChange(event)}
+            type="time"
             className="mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
           />
         </div>
@@ -113,3 +120,24 @@ export default function AddActivityForm(props) {
     </section>
   );
 }
+
+// CREATE TABLE "attractions" (
+//   "id" serial PRIMARY KEY NOT NULL,
+//   "name" varchar(255) NOT NULL,
+//   "description" text NOT NULL,
+//   "category" attraction_type NOT NULL,
+//   "image" text,
+//   "address" text NOT NULL,
+//   "location" point NOT NULL,
+//   "visible" boolean NOT NULL DEFAULT true
+// );
+
+// DROP TABLE IF EXISTS activities CASCADE;
+// CREATE TABLE "activities" (
+//   "id" serial PRIMARY KEY NOT NULL,
+//   "day_id" int,
+//   "start_time" time,
+//   "end_time" time,
+//   "attraction_id" int NOT NULL,
+//   "itinerary_id" int NOT NULL
+// );
