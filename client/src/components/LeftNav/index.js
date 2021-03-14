42cce9fec850f53540f4c6a5981fc445048d8cc5
May C <maycheongs@gmail.com>
2021-03-14 18:04:11 -0400

    remove dispatch from leftnav props

--- client/src/components/LeftNav/index.js
+++ client/src/components/LeftNav/index.js
@@ -3,19 +3,17 @@ import ItineraryLeftNav from './ItineraryLeftNav';
 import { useParams } from 'react-router-dom';
 
 export default function LeftNav(props) {
-
-  
   return (
     <div>
-      {useParams().itinerary_id ? 
+      {useParams().itinerary_id ? (
         <ItineraryLeftNav
           itinerary={props.itinerary}
           user={props.user}
-          dispatch={props.dispatch} 
-          setItinerary={props.setItinerary}/>       
-       : 
+          setItinerary={props.setItinerary}
+        />
+      ) : (
         <DashboardLeftNav user={props.user} />
-      }
+      )}
     </div>
   );
 }
