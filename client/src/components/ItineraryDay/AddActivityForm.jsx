3a02c9257e6cff9e7c1df3f78714b1156284f9c2
Rebecca Chen <rebecca061996@gmail.com>
2021-03-14 13:19:28 -0400

    complete form for creating a new activity and setup set error to display errors based on blank fields

--- client/src/components/ItineraryDay/AddActivityForm.jsx
+++ client/src/components/ItineraryDay/AddActivityForm.jsx
@@ -8,6 +8,15 @@ export default function AddActivityForm(props) {
   const [activity, setActivity] = useState({
     start: '',
     end: '',
+    name: '',
+    description: '',
+    image: '',
+    category: '',
+    street: '',
+    city: '',
+    state: '',
+    country: '',
+    postal: '',
   });
 
   const [error, setError] = useState({
@@ -27,24 +36,47 @@ export default function AddActivityForm(props) {
   const save = event => {
     event.preventDefault();
 
-    console.log(activity);
-    // if (userInfo.email === '') {
-    //   setError({
-    //     ...error,
-    //     status: true,
-    //     message: 'Please enter an email.',
-    //   });
-    //   return;
-    // }
+    if (activity.name === '') {
+      setError({
+        ...error,
+        status: true,
+        message: 'Please enter an activity name.',
+      });
+      return;
+    }
 
-    // if (userInfo.password === '') {
-    //   setError({
-    //     ...error,
-    //     status: true,
-    //     message: 'Please enter a password.',
-    //   });
-    //   return;
-    // }
+    if (activity.description === '') {
+      setError({
+        ...error,
+        status: true,
+        message: 'Please enter a description.',
+      });
+      return;
+    }
+
+    if (activity.category === '') {
+      setError({
+        ...error,
+        status: true,
+        message: 'Please select a category for your activity.',
+      });
+      return;
+    }
+
+    if (
+      activity.street === '' ||
+      activity.city === '' ||
+      activity.state === '' ||
+      activity.country === '' ||
+      activity.postal === ''
+    ) {
+      setError({
+        ...error,
+        status: true,
+        message: 'Please enter all address fields.',
+      });
+      return;
+    }
 
     // props.onSave(userInfo.email, userInfo.password).then(res => {
     //   if (res.data.email) {
@@ -80,16 +112,112 @@ export default function AddActivityForm(props) {
       ></ErrorMessage>
       <form onSubmit={event => save(event)} className="flex flex-col">
         <div className="flex flex-col mx-8 my-6">
-          <label htmlFor="email" className="font-semibold">
-            Email
+          <label htmlFor="name" className="font-semibold">
+            Activity Name
           </label>
           <input
-            value={1}
-            name="email"
-            onChange={() => console.log('here')}
+            value={activity.name}
+            name="name"
+            onChange={event => handleChange(event)}
             type="text"
-            placeholder="Email"
+            placeholder="Climb Mt. Everest"
             className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
+          />
+          <label htmlFor="category" className="font-semibold">
+            Category
+          </label>
+          <select
+            name="category"
+            value={activity.category}
+            onChange={event => handleChange(event)}
+            className="mb-4 border-gray-300 rounded-md appearance-none last-name focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
+          >
+            <option defaultValue></option>
+            <option value="adult">Adult</option>
+            <option value="amusement">Amusement</option>
+            <option value="accomodation">Accomodation</option>
+            <option value="landmark">Landmark</option>
+            <option value="sport">Sport</option>
+            <option value="food">Food</option>
+            <option value="cultural">Cultural</option>
+            <option value="nature">Nature</option>
+          </select>
+          <label htmlFor="street" className="font-semibold">
+            Street Address
+          </label>
+          <input
+            value={activity.street}
+            name="street"
+            onChange={event => handleChange(event)}
+            type="text"
+            placeholder="123 Main Street"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
+          />
+          <label htmlFor="city" className="font-semibold">
+            City
+          </label>
+          <input
+            value={activity.city}
+            name="city"
+            onChange={event => handleChange(event)}
+            type="text"
+            placeholder="New York City"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
+          />
+          <label htmlFor="state" className="font-semibold">
+            State / Province
+          </label>
+          <input
+            value={activity.state}
+            name="state"
+            onChange={event => handleChange(event)}
+            type="text"
+            placeholder="New York"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
+          />
+          <label htmlFor="country" className="font-semibold">
+            Country
+          </label>
+          <input
+            value={activity.country}
+            name="country"
+            onChange={event => handleChange(event)}
+            type="text"
+            placeholder="USA"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
+          />
+          <label htmlFor="postal" className="font-semibold">
+            Zip / Postal Code
+          </label>
+          <input
+            value={activity.postal}
+            name="postal"
+            onChange={event => handleChange(event)}
+            type="text"
+            placeholder="12345"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
+          />
+          <label htmlFor="image" className="font-semibold">
+            Image
+          </label>
+          <input
+            value={activity.image}
+            name="image"
+            onChange={event => handleChange(event)}
+            type="url"
+            placeholder="Image URL"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
+          />
+          <label htmlFor="description" className="font-semibold">
+            Description
+          </label>
+          <textarea
+            value={activity.description}
+            name="description"
+            onChange={event => handleChange(event)}
+            type="text"
+            placeholder="Let's climb Mt. Everest in one day!"
+            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600 resize-none"
           />
           <label htmlFor="start" className="font-semibold">
             Start Time
@@ -112,32 +240,11 @@ export default function AddActivityForm(props) {
             className="mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
           />
         </div>
-
         <footer className="flex items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 rounded-b-xl">
-          <FormButton type="submit">Log in</FormButton>
+          <FormButton type="submit">Save</FormButton>
+          <FormButton type="button">Cancel</FormButton>
         </footer>
       </form>
     </section>
   );
 }
-
-// CREATE TABLE "attractions" (
-//   "id" serial PRIMARY KEY NOT NULL,
-//   "name" varchar(255) NOT NULL,
-//   "description" text NOT NULL,
-//   "category" attraction_type NOT NULL,
-//   "image" text,
-//   "address" text NOT NULL,
-//   "location" point NOT NULL,
-//   "visible" boolean NOT NULL DEFAULT true
-// );
-
-// DROP TABLE IF EXISTS activities CASCADE;
-// CREATE TABLE "activities" (
-//   "id" serial PRIMARY KEY NOT NULL,
-//   "day_id" int,
-//   "start_time" time,
-//   "end_time" time,
-//   "attraction_id" int NOT NULL,
-//   "itinerary_id" int NOT NULL
-// );
