5a68cd938a0ff026a411f8e44c6822277fd41d88
babs20 <thevillemc@gmail.com>
2021-03-14 12:12:39 -0400

    add basic layout to card view of day activities

--- client/src/components/ItineraryDay/ItineraryDay.jsx
+++ client/src/components/ItineraryDay/ItineraryDay.jsx
@@ -12,13 +12,13 @@ export default function ItineraryDay(props) {
     locations = itinerary.locations;
   }
 
-  const getCurrentDay = locations => {
+  const getCurrentDay = (locations) => {
     let currentDay;
     let currentLocation;
 
     if (locations) {
-      locations.forEach(location => {
-        location.days.forEach(day => {
+      locations.forEach((location) => {
+        location.days.forEach((day) => {
           if (day.day_order === Number(day_id)) {
             currentLocation = location;
             currentDay = day;
@@ -36,26 +36,31 @@ export default function ItineraryDay(props) {
   const location = dayInfo.currentLocation;
 
   return (
-    <div>
-      <h1>
-        Day {day && day.day_order}: {location && location.name}
-      </h1>
-      {day && day.activities && day.activities.length === 1 && (
-        <h2>1 Activity Planned</h2>
-      )}
-      {day && day.activities && day.activities.length !== 1 && (
-        <h2>
-          {day && day.activities && day.activities.length} Activities Planned
-        </h2>
-      )}
-      <button>Add Activity</button>
+    <section className='flex flex-col justify-center w-3/4 h-full mx-auto space-y-4'>
+      <header className='flex justify-between'>
+        <div className=''>
+          <h1>
+            Day {day && day.day_order}: {location && location.name}
+          </h1>
+          {day && day.activities && day.activities.length === 1 && (
+            <h2>1 Activity Planned</h2>
+          )}
+          {day && day.activities && day.activities.length !== 1 && (
+            <h2>
+              {day && day.activities && day.activities.length} Activities
+              Planned
+            </h2>
+          )}
+        </div>
+        <button>Add Activity</button>
+      </header>
       {day &&
         day.activities &&
-        day.activities.map(activity => {
+        day.activities.map((activity) => {
           return (
             <ItineraryDayActivities key={activity.id} activity={activity} />
           );
         })}
-    </div>
+    </section>
   );
 }
