e3a4ae51be7d04fe603b49c9dd270fb412d965b4
Rebecca Chen <rebecca061996@gmail.com>
2021-03-13 22:37:01 -0500

    add location and activity count for day

--- client/src/components/ItineraryDay/ItineraryDay.jsx
+++ client/src/components/ItineraryDay/ItineraryDay.jsx
@@ -4,18 +4,38 @@ export default function ItineraryDay(props) {
   const { day_id } = useParams();
 
   const { itinerary } = props;
-
   const locations = itinerary.locations;
+
+  const getCurrentDay = locations => {
+    let currentDay;
+    let currentLocation;
+
+    locations.forEach(location => {
+      location.days.forEach(day => {
+        if (day.day_order === Number(day_id)) {
+          currentLocation = location;
+          currentDay = day;
+        }
+      });
+    });
+
+    return { currentDay, currentLocation };
+  };
+
+  const dayInfo = getCurrentDay(locations);
+
+  const day = dayInfo.currentDay;
+  const location = dayInfo.currentLocation;
 
   return (
     <div>
-      {/* <h1>
-        Day {day}: {location}
+      <h1>
+        Day {day.day_order}: {location.name}
       </h1>
       {day.activities.length === 1 && <h2>1 Activity Planned</h2>}
       {day.activities.length !== 1 && (
         <h2>{day.activities.length} Activities Planned</h2>
-      )} */}
+      )}
     </div>
   );
 }
