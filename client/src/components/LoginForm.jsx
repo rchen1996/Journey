75f924fbe7ac6339f3c8c8db0a8ecf82a15bc161
babs20 <thevillemc@gmail.com>
2021-03-15 12:10:17 -0400

    edit log in form for better ux

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -71,47 +71,40 @@ export default function LoginForm(props) {
   };
 
   return (
-    <section className="w-full shadow-lg bg-gray-50 rounded-xl">
+    <section className='w-full shadow-lg bg-gray-50 rounded-xl'>
       <ErrorMessage
         isError={error.status}
         show={error.show}
         hide={error.hide}
         message={error.message}
       ></ErrorMessage>
-      <form onSubmit={event => save(event)} className="flex flex-col">
-        <div className="flex flex-col mx-8 my-6">
-          <label htmlFor="email" className="font-semibold">
+      <form onSubmit={event => save(event)} className='flex flex-col'>
+        <div className='flex flex-col mx-8 my-4'>
+          <label htmlFor='email' className='font-semibold'>
             Email
           </label>
           <input
             value={userInfo.email}
-            name="email"
+            name='email'
             onChange={handleChange}
-            type="email"
-            placeholder="Email"
-            className="mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
+            type='email'
+            placeholder='Email'
+            className='mb-4 border-gray-300 rounded-md appearance-none focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
           />
-          <label htmlFor="password" className="font-semibold">
+          <label htmlFor='password' className='font-semibold'>
             Password
           </label>
           <input
             value={userInfo.password}
-            name="password"
+            name='password'
             onChange={handleChange}
-            type="password"
-            placeholder="Password"
-            className="mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600"
+            type='password'
+            placeholder='Password'
+            className='mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
           />
         </div>
-
-        <footer className="flex items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 rounded-b-xl">
-          <span className="text-xs font-semibold">
-            Don't have an account? Sign up{' '}
-            <Link to="/signup" className="text-teal-600 hover:underline">
-              here!
-            </Link>
-          </span>
-          <FormButton type="submit">Log in</FormButton>
+        <footer className='flex items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 rounded-b-xl'>
+          <FormButton type='submit'>Log in</FormButton>
         </footer>
       </form>
     </section>
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -5,7 +5,7 @@ export default function Register(props) {
   return (
     <div className='flex flex-col items-center w-full h-full'>
       <div className='flex flex-col w-5/6 py-20 lg:w-1/2'>
-        <h1 className='ml-1 text-2xl font-bold '>Begin Your Journey!</h1>
+        <h1 className='ml-1 text-2xl font-bold'>Begin Your Journey!</h1>
         <span className='mt-2 mb-4 ml-1.5 text-xs font-semibold'>
           Already have an account? Sign in{' '}
           <Link to='/login' className='text-teal-600 hover:underline'>
