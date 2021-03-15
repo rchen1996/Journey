27449d4289c9f535e7628271dfd12fa670143178
May C <maycheongs@gmail.com>
2021-03-14 22:14:36 -0400

    Days links on left nav links to edit day view when in itinerary edit view

--- client/src/components/LeftNav/ItineraryLeftNav.jsx
+++ client/src/components/LeftNav/ItineraryLeftNav.jsx
@@ -80,7 +80,7 @@ export default function ItineraryLeftNav(props) {
                   {locationObj.days.map((day) => {
                     return (
                       <NavLink
-                        to={`/itineraries/${itinerary.id}/days/${day.day_order}`}
+                        to={`/itineraries/${itinerary.id}/days/${day.day_order}/${pathname.includes('edit') && 'edit'}`}
                         key={day.id}
                         activeClassName='bg-gray-200 bg-opacity-25'
                         className='px-4 py-2 hover:bg-gray-200 hover:bg-opacity-25 rounded-xl'
