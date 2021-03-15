68b89380c4e8c56d5c0fb1aedfbad7e5946b076a
Rebecca Chen <rebecca061996@gmail.com>
2021-03-15 11:46:13 -0400

    create components for Bookmarks list page

--- client/src/components/Bookmarks.jsx
+++ client/src/components/Bookmarks.jsx
@@ -1,3 +1,17 @@
+import BookmarksItem from './BookmarksItem';
+
 export default function Bookmarks(props) {
-  return <div></div>;
+  const parsedBookmarks = props.bookmarks.map(bookmark => (
+    <BookmarksItem key={bookmark.id} bookmark={bookmark} />
+  ));
+  return (
+    <div className='flex flex-col w-full'>
+      <h1 className='pl-4 mt-8 ml-10 text-2xl font-bold border-l-8 border-teal-600'>
+        My Bookmarks
+      </h1>
+      <section className='grid gap-4 m-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-rows'>
+        {parsedBookmarks}
+      </section>
+    </div>
+  );
 }
