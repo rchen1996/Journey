eb7cd37fc7b959b1bad7ccc3c182c4d2fce956c7
babs20 <thevillemc@gmail.com>
2021-03-15 11:55:24 -0400

    edit form button

--- client/src/components/FormButton.jsx
+++ client/src/components/FormButton.jsx
@@ -3,7 +3,7 @@ export default function FormButton(props) {
     <button
       type={props.type}
       onClick={props.onClick}
-      className="px-4 py-3 font-semibold leading-none text-gray-200 bg-teal-600 border-2 border-transparent hover:text-teal-600 rounded-xl hover:border-teal-600 hover:bg-transparent focus:ring-teal-600 focus:ring-1"
+      className='w-1/5 px-4 py-3 font-semibold leading-none text-gray-200 bg-teal-600 border-2 border-transparent hover:text-teal-600 rounded-xl hover:border-teal-600 hover:bg-transparent focus:ring-teal-600 focus:ring-1'
     >
       {props.children}
     </button>
