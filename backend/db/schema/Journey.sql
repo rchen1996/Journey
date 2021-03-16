4943c8a2bb8fe5ce327e0229cdf0c8511b6eed1f
Rebecca Chen <rebecca061996@gmail.com>
2021-03-16 14:15:55 -0400

    fix syntax error in create table

--- backend/db/schema/Journey.sql
+++ backend/db/schema/Journey.sql
@@ -40,7 +40,7 @@ CREATE TABLE "itineraries" (
   "trip_type" traveler_type,
   "visible" boolean NOT NULL DEFAULT false,
   "creator_id" int NOT NULL,
-  "start_date" date,
+  "start_date" date
 );
 
 DROP TABLE IF EXISTS travel_parties CASCADE;
