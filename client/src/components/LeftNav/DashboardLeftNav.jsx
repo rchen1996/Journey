2d2d6dc0c26561311e9493c240166feca2d2737e
Rebecca Chen <rebecca061996@gmail.com>
2021-03-16 11:27:31 -0400

    add manage account button to dashboard left nav

--- client/src/components/LeftNav/DashboardLeftNav.jsx
+++ client/src/components/LeftNav/DashboardLeftNav.jsx
@@ -61,6 +61,27 @@ export default function DashboardLeftNav(props) {
             </svg>
           </NavLink>
         </div>
+        <div className='mb-4'>
+          <NavLink
+            to={`/dashboard/${user.id}/edit`}
+            activeClassName='selected bg-gray-200 bg-opacity-25'
+            className='flex items-center justify-between p-4 sm:px-4 sm:py-2 hover:bg-gray-200 hover:bg-opacity-25 rounded-xl'
+          >
+            <span className='hidden sm:flex'>Manage Account</span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              className='w-6 h-6 sm:ml-4'
+            >
+              <path
+                fillRule='evenodd'
+                d='M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
