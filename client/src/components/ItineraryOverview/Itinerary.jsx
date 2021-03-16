1a3e4aed974b2cc6b55d86811168cc74f1afc479
Rebecca Chen <rebecca061996@gmail.com>
2021-03-16 13:59:59 -0400

    show itinerary description/name/dates on overview

--- client/src/components/ItineraryOverview/Itinerary.jsx
+++ client/src/components/ItineraryOverview/Itinerary.jsx
@@ -3,8 +3,25 @@ import ItineraryDays from './ItineraryDays';
 export default function Itinerary(props) {
   const { itinerary, user, deleteDayFromItinerary } = props;
 
+  const formatDate = dateString => {
+    if (dateString) {
+      return new Date(dateString).toDateString();
+    }
+  };
+
   return (
-    <div className='flex w-full lg:ml-64'>
+    <div className='flex flex-col w-full lg:ml-64 pt-16'>
+      <h1>{itinerary && itinerary.name}</h1>
+      <h4>Trip Dates</h4>
+      <p>
+        {itinerary &&
+          `${formatDate(itinerary.start_date)} - ${formatDate(
+            itinerary.end_date
+          )}`}
+      </p>
+      <h4>Itinerary Description</h4>
+      <p>{itinerary && itinerary.description}</p>
+      <h2>Itinerary Overview</h2>
       <section className='flex flex-col w-full h-full mx-8 my-8 mt-24 lg:mx-24'>
         {itinerary &&
           itinerary.locations &&
