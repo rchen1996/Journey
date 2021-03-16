c420995583c98c65453a6382f9c05c7142444c6b
babs20 <thevillemc@gmail.com>
2021-03-16 14:01:30 -0400

    change error message location on form

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -16,8 +16,8 @@ export default function RegisterForm(props) {
   const [error, setError] = useState({
     staus: false,
     message: '',
-    show: 'flex p-3 mx-8 mt-8 bg-red-700 bg-opacity-50 rounded-xl',
-    hide: 'hidden flex p-3 mx-8 mt-8 bg-red-700 bg-opacity-50 rounded-xl',
+    show: 'flex p-3 bg-red-700 bg-opacity-50 rounded-xl mb-4',
+    hide: 'hidden',
   });
 
   const history = useHistory();
@@ -100,85 +100,90 @@ export default function RegisterForm(props) {
   };
 
   return (
-    <section className='w-full shadow-lg bg-gray-50 rounded-xl'>
+    <>
       <AlertMessage
         isError={error.status}
         show={error.show}
         hide={error.hide}
         message={error.message}
       ></AlertMessage>
-      <form
-        autoComplete='off'
-        onSubmit={event => save(event)}
-        className='flex flex-col'
-      >
-        <div className='flex flex-col mx-8 my-6'>
-          <label htmlFor='first-name' className='ml-1 font-semibold'>
-            First Name
-          </label>
-          <input
-            className='mb-4 border-gray-300 rounded-md appearance-none first-name focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
-            name='first-name'
-            type='text'
-            placeholder='First Name'
-            value={firstName}
-            onChange={event => setFirstName(event.target.value)}
-          />
-          <label htmlFor='last-name' className='ml-1 font-semibold'>
-            Last Name
-          </label>
-          <input
-            className='mb-4 border-gray-300 rounded-md appearance-none last-name focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
-            name='last-name'
-            type='text'
-            placeholder='Last Name'
-            value={lastName}
-            onChange={event => setLastName(event.target.value)}
-          />
-          <label htmlFor='email' className='ml-1 font-semibold'>
-            Email
-          </label>
-          <input
-            className='mb-4 border-gray-300 rounded-md appearance-none email focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
-            name='email'
-            type='email'
-            placeholder='Email'
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <label htmlFor='password' className='ml-1 font-semibold'>
-            Password
-          </label>
-          <input
-            className='mb-4 border-gray-300 rounded-md appearance-none password focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
-            name='password'
-            type='password'
-            value={password}
-            placeholder='Password'
-            onChange={event => setPassword(event.target.value)}
-          />
-          <label htmlFor='password-confirmation' className='ml-1 font-semibold'>
-            Confirm Password
-          </label>
-          <input
-            className='border-gray-300 rounded-md appearance-none password-confirmation focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
-            name='password-confirmation'
-            type='password'
-            placeholder='Confirm Password'
-            value={passwordConfirm}
-            onChange={event => setPasswordConfirm(event.target.value)}
-          />
-        </div>
-        <footer className='flex flex-col items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 sm:items-center sm:flex-row rounded-b-xl'>
-          <FormButton type='submit'>Sign up</FormButton>
-          <span className='mt-2 ml-1.5 text-xs font-semibold'>
-            Already have an account? Sign in{' '}
-            <Link to='/login' className='text-teal-600 hover:underline'>
-              here!
-            </Link>
-          </span>
-        </footer>
-      </form>
-    </section>
+      <section className='w-full shadow-lg bg-gray-50 rounded-xl'>
+        <form
+          autoComplete='off'
+          onSubmit={event => save(event)}
+          className='flex flex-col'
+        >
+          <div className='flex flex-col mx-8 my-6'>
+            <label htmlFor='first-name' className='ml-1 font-semibold'>
+              First Name
+            </label>
+            <input
+              className='mb-4 border-gray-300 rounded-md appearance-none first-name focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
+              name='first-name'
+              type='text'
+              placeholder='First Name'
+              value={firstName}
+              onChange={event => setFirstName(event.target.value)}
+            />
+            <label htmlFor='last-name' className='ml-1 font-semibold'>
+              Last Name
+            </label>
+            <input
+              className='mb-4 border-gray-300 rounded-md appearance-none last-name focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
+              name='last-name'
+              type='text'
+              placeholder='Last Name'
+              value={lastName}
+              onChange={event => setLastName(event.target.value)}
+            />
+            <label htmlFor='email' className='ml-1 font-semibold'>
+              Email
+            </label>
+            <input
+              className='mb-4 border-gray-300 rounded-md appearance-none email focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
+              name='email'
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+            />
+            <label htmlFor='password' className='ml-1 font-semibold'>
+              Password
+            </label>
+            <input
+              className='mb-4 border-gray-300 rounded-md appearance-none password focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
+              name='password'
+              type='password'
+              value={password}
+              placeholder='Password'
+              onChange={event => setPassword(event.target.value)}
+            />
+            <label
+              htmlFor='password-confirmation'
+              className='ml-1 font-semibold'
+            >
+              Confirm Password
+            </label>
+            <input
+              className='border-gray-300 rounded-md appearance-none password-confirmation focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
+              name='password-confirmation'
+              type='password'
+              placeholder='Confirm Password'
+              value={passwordConfirm}
+              onChange={event => setPasswordConfirm(event.target.value)}
+            />
+          </div>
+          <footer className='flex flex-col items-center justify-between px-8 py-3 bg-gray-300 bg-opacity-50 sm:items-center sm:flex-row rounded-b-xl'>
+            <FormButton type='submit'>Sign up</FormButton>
+            <span className='mt-2 ml-1.5 text-xs font-semibold'>
+              Already have an account? Sign in{' '}
+              <Link to='/login' className='text-teal-600 hover:underline'>
+                here!
+              </Link>
+            </span>
+          </footer>
+        </form>
+      </section>
+    </>
   );
 }
