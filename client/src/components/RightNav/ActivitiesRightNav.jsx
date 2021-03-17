ecb15c8805143f2a64f105f98f0c7e5e92f085e0
Rebecca Chen <rebecca061996@gmail.com>
2021-03-17 11:41:50 -0400

    add classes for activities right nav setup

--- client/src/components/RightNav/ActivitiesRightNav.jsx
+++ client/src/components/RightNav/ActivitiesRightNav.jsx
@@ -1,3 +1,9 @@
 export default function ActivitiesRightNav(props) {
-  return <div className='pt-16'>Hello</div>;
+  return (
+    <nav className='w-16 h-full mt-16 bg-gray-600 sm-w64'>
+      <div className='sticky flex flex-col items-center w-full px-4 py-6 text-xl font-semibold text-gray-100 sm:block top-16'>
+        <div className='mb-4'>Hello</div>
+      </div>
+    </nav>
+  );
 }
