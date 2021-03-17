423a8c3a4a513229e1dfb81001d764842b5ef7d8
Rebecca Chen <rebecca061996@gmail.com>
2021-03-17 18:31:00 -0400

    remove form button component - no longer used

--- client/src/components/RightNav/AttractionSearch.jsx
+++ client/src/components/RightNav/AttractionSearch.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import FormButton from '../FormButton';
 import Checkbox from './Checkbox';
 
 export default function AttractionSearch(props) {
