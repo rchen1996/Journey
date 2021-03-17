a8b76768bdd6ae4d8a8a065c576fee40e1613d62
Rebecca Chen <rebecca061996@gmail.com>
2021-03-17 11:59:55 -0400

    add buttons for the different views on the right nav and setup changing views

--- client/src/components/RightNav/ActivitiesRightNav.jsx
+++ client/src/components/RightNav/ActivitiesRightNav.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+
 export default function ActivitiesRightNav(props) {
+  const MY_LOCATIONS = 'MY_LOCATIONS';
+  const ATTRACTION_SEARCH = 'ACTIVITIES_SEARCH';
+
+  const [view, setView] = useState(MY_LOCATIONS);
+
   return (
-    <nav className='w-16 h-full mt-16 bg-gray-600 sm-w64'>
+    <nav className='mt-16 bg-gray-600 sm-w64'>
       <div className='sticky flex flex-col items-center w-full px-4 py-6 text-xl font-semibold text-gray-100 sm:block top-16'>
-        <div className='mb-4'>Hello</div>
+        <div className='mb-4'>
+          <button
+            type='button'
+            onClick={() => setView(MY_LOCATIONS)}
+            className='flex items-center justify-between p-4 sm:px-4 sm:py-2 hover:bg-gray-200 hover:bg-opacity-25 rounded-xl'
+          >
+            My Locations
+          </button>
+          <button
+            type='button'
+            onClick={() => setView(ATTRACTION_SEARCH)}
+            className='flex items-center justify-between p-4 sm:px-4 sm:py-2 hover:bg-gray-200 hover:bg-opacity-25 rounded-xl'
+          >
+            Search for Attractions
+          </button>
+        </div>
       </div>
     </nav>
   );
