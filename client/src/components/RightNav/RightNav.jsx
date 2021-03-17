0d3359bdf9d5098dd8067352a97abb6136d50325
babs20 <thevillemc@gmail.com>
2021-03-17 19:07:37 -0400

    adjust views with edit to work with right nav

--- client/src/components/RightNav/RightNav.jsx
+++ client/src/components/RightNav/RightNav.jsx
@@ -9,7 +9,7 @@ export default function RightNav(props) {
   return (
     <nav
       className={
-        isRightNavOpen
+        url.includes('edit') && isRightNavOpen
           ? 'fixed -right-0 bg-gray-600 z-40 w-full h-full px-4 py-4 pb-24 space-y-2 overflow-y-scroll mt-16 text-gray-100 divide-y divide-gray-100 lg:w-64 md:w-full no-scrollbar lg:block divide-opacity-50'
           : 'hidden'
       }
