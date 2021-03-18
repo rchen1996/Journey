313fdd3fde1ef1368fae04de3d3c9583de8eb930
Rebecca Chen <rebecca061996@gmail.com>
2021-03-17 20:59:58 -0400

    fix checkboxes to be controlled input after being put into their own component

--- client/src/components/RightNav/Checkbox.jsx
+++ client/src/components/RightNav/Checkbox.jsx
@@ -1,17 +1,19 @@
 export default function Checkbox(props) {
   const { name, category, handleCategoryChange } = props;
+  const lowerCaseName = name.toLowerCase();
+
   return (
     <div>
       <input
         type='checkbox'
-        id={name}
-        name={name}
-        value={name}
-        checked={category[name]}
+        id={lowerCaseName}
+        name={lowerCaseName}
+        value={lowerCaseName}
+        checked={category[lowerCaseName]}
         onChange={event => handleCategoryChange(event)}
         className='mr-2 text-teal-600 border-gray-300 rounded focus:ring-teal-500 ring-offset-0'
       />
-      <label htmlFor={name} className='text-gray-100'>
+      <label htmlFor={lowerCaseName} className='text-gray-100'>
         {name}
       </label>
     </div>
