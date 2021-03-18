5fead62ebd1582d5f0fb8af3c88389e785e3390c
babs20 <thevillemc@gmail.com>
2021-03-18 15:56:20 -0400

    add footer content

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -281,14 +281,6 @@ export default function Home() {
           <div className='flex-none w-full h-64 transform shadow-lg sm:h-44 bg-gray-50 rounded-2xl rotate-3'></div>
           <div className='flex flex-col justify-center w-full h-64 p-6 -mt-64 space-y-2 text-gray-200 transform bg-teal-600 shadow-lg sm:h-44 sm:-mt-44 rounded-2xl'>
             <h2 className='flex items-center space-x-2 text-4xl font-black'>
-              {/* <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='z-50 w-12 h-12 mr-2'
-              >
-                <path d='M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z' />
-              </svg> */}
               Get Inspired
             </h2>
             <p className='text-xl font-light leading-snug'>
@@ -299,7 +291,55 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer className='h-32 text-gray-200 bg-gray-600'>PAIN</footer>
+      <footer className='flex items-center justify-between h-32 p-8 text-gray-200 bg-gray-600 shadow-md'>
+        <div className='space-y-2'>
+          <h6>Follow us on social media</h6>
+          <div className='flex justify-between px-2'>
+            <svg
+              ariaHidden='true'
+              focusable='false'
+              dataPrefix='fab'
+              dataIcon='instagram'
+              className='w-8 h-8 text-gray-400 cursor-pointer hover:text-gray-100'
+              role='img'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 448 512'
+              fill='currentColor'
+            >
+              <path d='M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z'></path>
+            </svg>
+            <svg
+              aria-hidden='true'
+              focusable='false'
+              data-prefix='fab'
+              data-icon='twitter'
+              className='w-8 h-8 text-gray-400 cursor-pointer hover:text-gray-100'
+              role='img'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 512 512'
+              fill='currentColor'
+            >
+              <path d='M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z'></path>
+            </svg>
+            <svg
+              aria-hidden='true'
+              focusable='false'
+              data-prefix='fab'
+              data-icon='facebook-f'
+              className='w-8 h-8 text-gray-400 cursor-pointer hover:text-gray-100'
+              role='img'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 320 512'
+            >
+              <path
+                fill='currentColor'
+                d='M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z'
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <span className='text-xs'>Copyright 2021</span>
+      </footer>
     </main>
   );
 }
