939db34c89870b27d0d83a944450390192e44283
Rebecca Chen <rebecca061996@gmail.com>
2021-03-18 15:22:59 -0400

    add frontend logic for adding a dayid to an activity

--- client/src/components/RightNav/MyLocationsItem.jsx
+++ client/src/components/RightNav/MyLocationsItem.jsx
@@ -1,9 +1,31 @@
+import { useParams } from 'react-router-dom';
+import { SET_ITINERARY } from '../../reducers/application';
+
 export default function MyLocationsItem(props) {
-  const { location } = props;
+  const { location, itinerary, updateActivityDay, dispatch } = props;
+
+  const { day_id } = useParams();
+
+  const addToDay = () => {
+    updateActivityDay(location.activity_id, day_id, itinerary.id).then(res => {
+      if (res.data.error) {
+        // display error => must be logged in or don't have permissions because not part of travel party
+      } else {
+        dispatch({
+          type: SET_ITINERARY,
+          itinerary: { ...itinerary, ...res.data },
+        });
+      }
+    });
+  };
+
+  const removeLocation = () => {
+    // delete the activity given an activity id
+  };
 
   return (
     <div
-      className='z-0 flex flex-col w-full mt-4 rounded-xl module group'
+      className='z-0 flex flex-col w-full mt-4 rounded-xl module'
       style={{ backgroundImage: `url(${location.image})` }}
     >
       <div className='w-full transition bg-gray-800 shadow-lg bg-opacity-80 xl:block rounded-xl module-inside group-hover:bg-gray-100'>
@@ -12,6 +34,15 @@ export default function MyLocationsItem(props) {
           <p className='text-sm font-normal leading-tight text-gray-300'>
             {location.description}
           </p>
+        </div>
+
+        <div>
+          <button type='button' onClick={addToDay}>
+            Add to Day
+          </button>
+          <button type='button' onClick={removeLocation}>
+            Delete from My Locations
+          </button>
         </div>
       </div>
     </div>
