4fef21e2811036c7bcbc14b4d9477b648ac7ce99
Rebecca Chen <rebecca061996@gmail.com>
2021-03-19 12:28:54 -0400

    add filter button that changes show/hide view for advanced search

--- client/src/components/ItineraryList.jsx
+++ client/src/components/ItineraryList.jsx
@@ -5,6 +5,29 @@ import ItineraryListItem from './ItineraryListItem';
 export default function ItineraryList(props) {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const HIDE = 'HIDE';
+  const SHOW = 'SHOW';
+  const [view, setView] = useState(HIDE);
+
+  const advancedSearchView = () => {
+    if (view === HIDE) {
+      setView(SHOW);
+    } else {
+      setView(HIDE);
+    }
+  };
+
+  const tripTypes = [
+    'Couples',
+    'Groups',
+    'Families',
+    'Solo',
+    'Backpackers',
+    'Luxury',
+    'Business',
+    'Accessibility',
+  ];
+
   const parsedItineraries = props.itineraries.map(itinerary => (
     <ItineraryListItem
       key={itinerary.id}
@@ -18,22 +41,42 @@ export default function ItineraryList(props) {
 
   const search = event => {
     event.preventDefault();
+    // default loads in to be a list of 20-25 itineraries (most popular based on bookmarks)
+    // always display based on popularity
+
+    // typing into the search bar searches the locations, itinerary name, itinerary description
+
+    // advanced search options:
+    // trip type - couples, groups, families, solo, backpackers, business, accessibility, luxury
+    // trip length - slider? incrementor? - search results will display trips with +- 2 days
   };
 
   return (
     <div className='pt-16'>
       <h1>Explore</h1>
       <form onSubmit={search}>
-        <h4>Search By:</h4>
-        <p>Location, Itinerary Name, Popularity, Trip Length, Trip Type</p>
         <input
           value={searchTerm}
           name='search'
           onChange={event => setSearchTerm(event.target.value)}
           type='text'
-          placeholder='Search for an itinerary'
+          placeholder='Search'
           className='mb-2 border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
         />
+        <button type='button' onClick={advancedSearchView}>
+          Filter
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path
+              fillRule='evenodd'
+              d='M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </button>
         <button
           type='submit'
           className='flex justify-between px-4 py-2 font-semibold text-gray-200 bg-teal-600 border-2 border-transparent rounded-xl focus:ring-teal-600 focus:ring-1'
