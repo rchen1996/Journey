010f4e746fe399b9228e5ceb3073fb2162e45936
babs20 <thevillemc@gmail.com>
2021-03-19 23:08:56 -0400

    center search bar

--- client/src/components/ItineraryList.jsx
+++ client/src/components/ItineraryList.jsx
@@ -132,63 +132,69 @@ export default function ItineraryList(props) {
   };
 
   return (
-    <div className='w-full h-full p-8 mt-16'>
-      <div className='pl-3 border-l-8 border-teal-600 '>
-        <h1 className='text-3xl font-bold'>Explore</h1>
-        <h2 className='text-lg text-gray-500'>
-          Get inspired by other members' explorations and adventures
-        </h2>
-      </div>
-      <form onSubmit={search} className='flex flex-col w-full mt-4 mb-2'>
-        <div className='flex w-full mb-2 md:w-3/4 lg:w-1/2'>
-          <div className='flex items-center w-full mr-4'>
-            <input
-              value={searchTerm}
-              name='search'
-              onChange={event => setSearchTerm(event.target.value)}
-              type='text'
-              placeholder='Search'
-              className='w-full border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
-            />
+    <div className='w-full h-full pb-8 mt-16'>
+      {/* <div className='flex flex-col items-center pt-12 pb-4 space-y-2'></div> */}
+      <form
+        onSubmit={search}
+        className='flex flex-col items-center w-full py-8 mb-8 shadow-md bg-gradient-to-b to-teal-600 from-blue-400'
+      >
+        <div className='flex flex-col w-3/4 p-4 py-6 mb-2 bg-gray-100 shadow-md md:w-3/4 lg:w-1/2 rounded-xl'>
+          <div className='flex flex-col items-center pb-3 md:-ml-4'>
+            <h1 className='pb-1 text-5xl font-black'>Explore</h1>
+            <h2 className='text-center text-gray-500'>
+              Get inspired by other members' explorations and adventures
+            </h2>
+          </div>
+          <div className='flex'>
+            <div className='flex items-center w-full mr-4'>
+              <input
+                value={searchTerm}
+                name='search'
+                onChange={event => setSearchTerm(event.target.value)}
+                type='text'
+                placeholder='Search'
+                className='w-full border-gray-300 rounded-md focus:ring-teal-600 focus:ring-1 focus:border-teal-600'
+              />
+              <button
+                type='button'
+                onClick={advancedSearchView}
+                className='-ml-8'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                  className='w-5 h-5'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </button>
+            </div>
             <button
-              type='button'
-              onClick={advancedSearchView}
-              className='-ml-8'
+              type='submit'
+              className='flex justify-between px-4 py-2 font-semibold text-gray-200 bg-teal-600 border-2 border-transparent rounded-xl focus:ring-teal-600 focus:ring-1'
             >
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 20 20'
                 fill='currentColor'
-                className='w-5 h-5'
+                className='w-10 h-5'
               >
                 <path
                   fillRule='evenodd'
-                  d='M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z'
+                  d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
                   clipRule='evenodd'
                 />
               </svg>
             </button>
           </div>
-          <button
-            type='submit'
-            className='flex justify-between px-4 py-2 font-semibold text-gray-200 bg-teal-600 border-2 border-transparent rounded-xl focus:ring-teal-600 focus:ring-1'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              className='w-10 h-5'
-            >
-              <path
-                fillRule='evenodd'
-                d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </button>
         </div>
         {view === SHOW && (
-          <div className='z-50 flex flex-col w-full my-2 bg-gray-100 divide-y shadow-xl md:mt-12 md:absolute md:w-3/4 lg:w-1/2 rounded-xl'>
+          <div className='z-50 flex flex-col w-full my-2 bg-gray-100 divide-y shadow-xl md:mt-48 md:absolute md:w-3/4 lg:w-1/2 rounded-xl'>
             <h4 className='px-3 pt-3 font-bold'>Trip Type</h4>
             <div className='grid grid-cols-2 grid-rows-4 px-3 pt-3 my-2 sm:grid-cols-4 sm:grid-rows-2'>
               {tripTypes.map((tripType, index) => {
@@ -245,7 +251,7 @@ export default function ItineraryList(props) {
         </div>
       )}
       {loading === false && parsedItineraries.length > 0 && (
-        <section className='grid w-full gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-flow-rows'>
+        <section className='grid w-full gap-4 px-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-flow-rows'>
           {parsedItineraries}
         </section>
       )}
