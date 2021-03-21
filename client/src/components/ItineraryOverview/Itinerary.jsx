9c44930778539e35d9d33943297e1b0efe6103fa
Rebecca Chen <rebecca061996@gmail.com>
2021-03-21 13:47:59 -0400

    add edit and delete buttons for each trip note

--- client/src/components/ItineraryOverview/Itinerary.jsx
+++ client/src/components/ItineraryOverview/Itinerary.jsx
@@ -292,8 +292,39 @@ export default function Itinerary(props) {
             {pinnedNotes.length > 0 &&
               pinnedNotes.map(note => {
                 return (
-                  // image of thumbtack/pin
-                  <p>{note.note}</p>
+                  <div>
+                    {/* image of thumbtack/pin */}
+                    <p>{note.note}</p>
+                    {url.includes('edit') && (
+                      <div>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'
+                          className='w-5 h-5'
+                        >
+                          <path d='M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z' />
+                          <path
+                            fillRule='evenodd'
+                            d='M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z'
+                            clipRule='evenodd'
+                          />
+                        </svg>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'
+                          className='w-5 h-5'
+                        >
+                          <path
+                            fillRule='evenodd'
+                            d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
+                            clipRule='evenodd'
+                          />
+                        </svg>
+                      </div>
+                    )}
+                  </div>
                 );
               })}
           </div>
@@ -301,8 +332,39 @@ export default function Itinerary(props) {
             regularNotes.length > 0 &&
             regularNotes.map(note => {
               return (
-                // image of paperclip
-                <p>{note.note}</p>
+                <div>
+                  {/* image of paperclip */}
+                  <p>{note.note}</p>
+                  {url.includes('edit') && (
+                    <div>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='w-5 h-5'
+                      >
+                        <path d='M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z' />
+                        <path
+                          fillRule='evenodd'
+                          d='M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='w-5 h-5'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                    </div>
+                  )}
+                </div>
               );
             })}
           {regularNotes.length === 0 && pinnedNotes.length === 0 && (
